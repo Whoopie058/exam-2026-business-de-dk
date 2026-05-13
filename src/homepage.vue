bc9f052438980d34a7c930a43e5dcbf0bd0426c9
Whoopie058 <lauhan05@easv365.dk>
2026-05-13 12:19:54 +0200

    gg

--- src/homepage.vue
+++ src/homepage.vue
@@ -18,7 +18,8 @@
           Synlighed – Formidling af viden og analyser via en mediekanal
           Erhvervsråd – Undersøgelse af potentialet for et cross-border business-råd
           Arbejdskraft og vækst – Tiltrækning og fastholdelse af talent i regionen
-          Business DE-DK skaber overblik, faciliterer samarbejde og styrker den grænseoverskridende erhvervsudvikling.</p>
+          Business DE-DK skaber overblik, faciliterer samarbejde og styrker den grænseoverskridende erhvervsudvikling.
+        gjgj</p>
       </section>
 
       <section class="image-section">
